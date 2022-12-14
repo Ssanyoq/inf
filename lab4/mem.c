1a575663eb6e4e4dd5bba8d58ea23fee9f623d88
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-14 12:13:40 +0300

    Typos fixed

--- lab4/mem.c
+++ lab4/mem.c
@@ -5,7 +5,7 @@
 char *safe_malloc(int len) {
     char *str = (char*)malloc(len * sizeof(char));
     if (!str) {
-        printf("Error occured while allocating memory\n");
+        printf("Error occurred while allocating memory\n");
         return NULL;
     }
     return str;
@@ -14,7 +14,7 @@ char *safe_malloc(int len) {
 char *safe_realloc(char *str, int len) {
     str = (char *)realloc(str, len * sizeof(char));
     if (!str) {
-        printf("Error occured while allocating memory\n");
+        printf("Error occurred while allocating memory\n");
         return NULL;
     }
     return str;
