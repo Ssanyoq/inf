df6048c85e13da4ba21b398bae3f0e9d6fb2be13
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-21 08:02:26 +0300

    Infinite readline

--- lab4/my/myreadline.c
+++ lab4/my/myreadline.c
@@ -1,16 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../mem.h"
+#include "mystring.h"
+
+#define BUFSIZE 256
 
 
 char *readline(const char *prompt) {
-    int limit = 256;
-    char *str = safe_malloc(limit);
+    char *str = safe_malloc(0);
+    int len = 0;
     printf("%s", prompt);
-    int out = scanf("%255[^\n]", str);
-    if (out == -1) {
-        return NULL;
+    char *buf = safe_malloc(BUFSIZE);
+    int buf_len;
+    int out;
+    while (1) {
+        out = scanf("%.[^\n]%n", BUFSIZE - 1, buf, buf_len);
+        if (out == -1) {
+            return NULL;
+        }
+        if (out == 0) {
+            break;
+        }
+        len += buf_len;
+        str = safe_realloc(str, len);
+        str = strcat(str, buf);
+        free(buf);
     }
+
     scanf("%*c");
     return str;
 }
