9e45a5327cee4d15fcf0a6bd1671aac248989a82
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-22 20:56:07 +0300

    more options, no -q

--- lab5/main.c
+++ lab5/main.c
@@ -4,52 +4,61 @@
 #include "mystruct.h"
 
 // sort: q, o, s
-// args: <read file> <write file> -s <sorting type> -q <1|0 - quiet or not>\// -r <1|0 - reversed or not> -f <field for sorting>
+// args: <read file> <write file> -s <sorting type>\
+// -r <1|0 - reversed or not> -f <field for sorting>
 int main(int argc, char *argv[]) {
     int c;
     int sort_type = 0; // 0 - qsort, 1 - odd even, 2 - Shell
-    int 
-    int quiet = 0;
+    int reversed = 0;
+    int field = 0; // 0 - name, 1 - phone, 2 - 
     char arg;
-    while ((c = getopt(argc, argv, "s:q")) != -1) {
+    while ((c = getopt(argc, argv, "s:r:f")) != -1) {
         switch (c) {
             case 's':
                 was_sorting = 1;
                 arg = optarg[0];
                 switch (arg) {
-                case 'q':
-                    sort_type = 0;
-                    break;
-                case 'o':
-                    sort_type = 1;
-                    break;
-                case 's':
-                    sort_type = 2;
-                    break;
-                default:
-                    fprintf(stderr, "Error: sorting of this type does not exist\n");
+                    case 'q':
+                        sort_type = 0;
+                        break;
+                    case 'o':
+                        sort_type = 1;
+                        break;
+                    case 's':
+                        sort_type = 2;
+                        break;
+                    default:
+                        fprintf(stderr, "Error: sorting of this type does not exist\n");
+                        return 1;
+                break;
+                }
+            case 'r':
+                if (optarg[0] == '0' || optarg[0] == '1') {
+                    reversed = (int)optarg[0];
+                } else {
+                    fprintf(stderr, "Error: incorrect -r argument\n");
                     return 1;
                 }
-                break;
-            case 'q':
-                if (optarg[0] == '1' || optarg[0] == '2') {
-                    quiet = (int)optarg[0];
+            case 'f':
+                if (optarg[0] == '1' || optarg[0] == '2' || optarg[0] == '3') {
+                    sort_type = (int)optarg[0];
                 } else {
                     fprintf(stderr, "Error: incorrect -q argument\n");
                     return 1;
                 }
-                break;
             default:
                 fprintf(stderr, "Error: incorrect argument given\n");
                 return 1;
         }
-        printf("???\n");
     }
-    printf("%d\n",c);
 
     // postitional
 
     FILE *read_file = (FILE *)argv[optind];
+    if (!read_file) {
+        fprintf(stderr, "Error: missing required argument");
+        return 1;
+    }
     if (access((char *)read_file, F_OK) == 0) {
         // gut
     } else {
@@ -58,6 +67,10 @@ int main(int argc, char *argv[]) {
     }
 
     FILE *write_file = (FILE *)argv[optind];
+    if (!write_file) {
+        fprintf(stderr, "Error: missing required argument");
+        return 1;
+    }
     if (access((char *)write_file, F_OK) == 0) {
         // gut
     } else {
