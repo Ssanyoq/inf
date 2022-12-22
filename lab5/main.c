915582f15aebbeba6748944de860e5d6d6e28233
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-22 19:25:59 +0300

    switch with cmd args

--- lab5/main.c
+++ lab5/main.c
@@ -2,22 +2,46 @@
 #include <stdio.h>
 #include "mystruct.h"
 
-// sort: qs, oe, sh
-
-// args: <read file> <write file> -s <sorting type> -silent
+// sort: q, o, s
+// args: <read file> <write file> -s <sorting type> -q(quiet)
 
 int main(int argc, char **argv) {
-    int c
+    int c;
     int was_sorting = 0;
     int sort_type = 0; // 0 - qsort, 1 - odd even, 2 - Shell
+    int quiet = 0;
     while ((c = getopt(argc, argv, "")) != -1) {
         switch (c)
         {
         case 's':
-
+            was_sorting = 1;
+            switch (optarg)
+            {
+            case 'q':
+                sort_type = 0;
+                break;
+            case 'o':
+                sort_type = 1;
+                break;
+            case 's':
+                sort_type = 2;
+                break;
+            default:
+                fprintf(stderr, "Error: sorting of this type does not exist");
+                return 1;
+            }
+            break;
+        case 'q':
+            quiet = 1;
             break;
         default:
-            break;
+            fprintf(stderr, "Error: %c is an incorrect argument", c);
+            return 1;
         }
     }
+    if (!was_sorting) {
+        fprintf(stderr, "Error: sorting not specified");
+        return 1;
+    }
+    return 0;
 }
