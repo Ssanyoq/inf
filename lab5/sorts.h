67b11310053ec4330cd1fdbd9863ae0c8adb8568
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-23 11:00:41 +0300

    sorts header

--- lab5/sorts.h
+++ lab5/sorts.h
@@ -1,6 +1,16 @@
 #ifndef SORTS_H
 #define SORTS_H
 
+int compare_names(Subscriber a, Subscriber b);
 
+int compare_phones(Subscriber a, Subscriber b);
+
+int compare_timestamps(Subscriber a, Subscriber b);
+
+int make_int(char *phone);
+
+void qsortpp(Subscriber *arr, size_t size, int sort_type);
+
+void odd_even_sort(Subscriber *arr, size_t size, int(*compar)(Subscriber, Subscriber));
 
 #endif SORTS_H
