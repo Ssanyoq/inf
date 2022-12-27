f48411d5a033f9e164bdc37bc305142fc8b71166
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-27 17:26:05 +0300

    void realloc and malloc

--- lab5/mem.c
+++ lab5/mem.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include "mem.h"
 
-char *safe_malloc(int len) {
-    char *str = (char*)malloc(len * sizeof(char));
+void *safe_malloc(int len) {
+    char *str = (char*)malloc(len);
     if (!str) {
         printf("Error occurred while allocating memory\n");
         return NULL;
@@ -11,8 +11,8 @@ char *safe_malloc(int len) {
     return str;
 }
 
-char *safe_realloc(char *str, int len) {
-    str = (char *)realloc(str, len * sizeof(char));
+void *safe_realloc(char *str, int len) {
+    str = (char *)realloc(str, len);
     if (!str) {
         printf("Error occurred while allocating memory\n");
         return NULL;
