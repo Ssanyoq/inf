3d1ade381be928ad383611967dfb16506b513f82
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-28 00:14:15 +0300

    Seemingly flawless processgit commit -i main.c -m Seemingly

--- lab6/main.c
+++ lab6/main.c
@@ -7,28 +7,80 @@
 
 void process(List *l) {
     Item *cur = l->head;
-    int i;
-    int m_pos;
-    int was_sep = 1;
-    Item *m;
-    Item *last;
-    while (cur != NULL)
-    {
-        if (cur->symb == ' ') {
-        } else {
-            if (was_sep) { // new word
-                was_sep = 0;
-                i = 0;
-                last = cur;
-                m = cur;
-                m_pos = 0;
-            } else {
-                i++;
-                last = cur;
-                if (i - m_pos > N) {
-                    m_pos++;
-                    m = m->ptr;
+
+    Item *word_start = NULL;
+    Item *word_end = NULL;
+    Item *marker = NULL;
+    Item *symb_before_word = NULL;
+    int word_i;
+    int word_len = 0;
+    int marker_i;
+
+    while (cur != NULL) {
+        if (cur->symb != ' ') {
+            if (word_len != 0) {
+                word_end = cur;
+                if (word_i - marker_i > N) {
+                    marker = marker->ptr;
+                    marker_i++;
                 }
+            } else {
+                word_end = cur;
+                word_start = cur;
+                marker = cur;
+            }
+            word_len++;
+            word_i++;
+        } else {
+            if (word_len != 0) {
+                if (N < word_len) {
+                    // ez
+                } else {
+                    int cur_n = N % word_len;
+                    if (word_i - marker_i < cur_n) {
+                        marker_i = 0;
+                        marker = word_start;
+                    }
+                    while (word_i - marker_i != cur_n)
+                    {
+                        marker = marker->ptr;
+                        marker_i++;
+                    }
+                        
+                }
+                if (symb_before_word != NULL) {
+                    symb_before_word->ptr = marker->ptr;
+                } else {
+                    l->head = marker->ptr;
+                }
+                marker->ptr = cur;
+                word_end->ptr = word_start;
+                if (word_start == l->head) {
+                    l->head = marker;
+                }
+                word_len = 0;
+                marker_i = 0;
+                word_i = 0;
+
+            }
+            symb_before_word = cur;
+        }
+
+
+        cur = cur->ptr;
+    }
+    printf("normal\n");
+    if (word_len != 0) {
+        if (N < word_len) {
+            if (symb_before_word != NULL) {
+                symb_before_word->ptr = marker->ptr;
+            } else {
+                l->head = marker->ptr;
+            }
+            marker->ptr = cur;
+            word_end->ptr = word_start;
+            if (word_start == l->head) {
+                l->head = marker;
             }
         }
     }
@@ -38,13 +90,16 @@ void process(List *l) {
 int main() {
     List *list = read_list();
     while (list != NULL) {
-        printf("Given string: \"");
+        // printf("Given string: \"");
+        // print_list(list);
+        // printf("\"\n");
+        process(list);
+        printf("Processed: \"");
         print_list(list);
         printf("\"\n");
-        process(list);
-
         free_list(list);
         list = read_list();
+        printf("\n");
     }
     return 0;
 }
