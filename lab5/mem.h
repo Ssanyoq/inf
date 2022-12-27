d9517f3d774d228bf5e75af6f2593a77838f25f5
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-27 17:27:22 +0300

    void realloc and malloc

--- lab5/mem.h
+++ lab5/mem.h
@@ -1,8 +1,8 @@
 #ifndef MEM_H
 #define MEM_H
 
-char *safe_malloc(int len);
+void *safe_malloc(int len);
 
-char *safe_realloc(char *str, int len);
+void *safe_realloc(char *str, int len);
 
 #endif
