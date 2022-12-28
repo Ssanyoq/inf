02452f1062b13773dc0f80a49cf898cc0c7d409c
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2022-12-28 13:14:34 +0300

    reads generation and generated reads

--- lab5/plots.py
+++ lab5/plots.py
@@ -1,12 +1,22 @@
 import matplotlib as plt
 import os
 import random
+import string
+
 
 def generate_read(n: int):
-    with open("read.txt", "w") as file:
+    with open("reads/read" + str(n) + ".txt", "w") as file:
+        file.write(str(n) + '\n')
         for i in range(n):
-            
+            name = ''
+            phone = ''
+            for j in range(10):
+                name += random.choice(string.ascii_letters)
+                phone += random.choice(string.digits)
+            ts = random.randint(0, 1_000_000_000)
+            print(name, phone, ts, sep='\n', file=file)
 
 
-stream = os.popen("build/e read.txt write.txt")
+generate_read(10000)
+stream = os.fopen("build/e reads/read100.txt write.txt")
 print(stream.read())
