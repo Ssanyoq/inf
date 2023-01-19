e980940acf81f66a2cb9f92b535e111620ce67c2
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-01-19 16:20:44 +0300

    finished parameters parser

--- lab5/main.c
+++ lab5/main.c
@@ -2,30 +2,40 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define ERROR_CODE 1
+
 void err_bad_input() {
-    fprintf(stderr, "")
+    fprintf(stderr, "Wrong parameters given\n");
 }
 
 // sorts: -s q|o|s 
 // reversed: -r 1|0
 // field for sorting: -f 1|2|3
 // readfile, writefile
-int main(int argc, int **argv) {
-    int (*sort)(void *base, size_t nel, size_t width, int (*compar)(const void *, const void *)) = qsort;
+int main(int argc, char **argv) {
+    void (*sort)(void *base, size_t nel, size_t width, int (*compar)(const void *, const void *)) = qsort;
     int reversed = 0;
     int (*compar)(const void *, const void *);
     int c;
+    int field = 1; // 1 - name, 2 - phone, 3 - timestamp
     while ((c = getopt(argc, argv, "s:r:f")) != -1) {
         switch (c)
         {
         case 's': // sorting type
             switch (optarg[0])
             {
-            case /* constant-expression */:
-                /* code */
+            case 'q':
+                // TODO:
                 break;
-            
+            case 'o':
+                //TODO:
+                break;
+            case 's':
+                // TODO:
+                break;
             default:
+                err_bad_input();
+                return ERROR_CODE;
                 break;
             }
             break;
@@ -39,7 +49,8 @@ int main(int argc, int **argv) {
                 reversed = 0;
                 break;
             default:
-
+                err_bad_input();
+                return ERROR_CODE;
                 break;
             }
             break;
@@ -47,17 +58,61 @@ int main(int argc, int **argv) {
             switch (optarg[0])
             {
             case '1':
+                field = 1;
                 break;
             case '2':
+                field = 2;
                 break;
             case '3':
+                field = 3;
                 break;
             default:
+                err_bad_input();
+                return ERROR_CODE;
                 break;
             }
             break;
         default: // bad input
+            err_bad_input();
+            return ERROR_CODE;
             break;
         }
+    } // optargs end
+
+    if (optind >= argc) {
+        err_bad_input();
+        return ERROR_CODE;
     }
+    
+    char *read_file = argv[optind];
+    if (!read_file) {
+        err_bad_input();
+        return ERROR_CODE;
+    }
+    if (access(read_file, F_OK) == 0) {
+        // gut
+    } else {
+        fprintf(stderr, "Error: read file does not exist\n");
+        return ERROR_CODE;
+    }
+    optind++;
+    if (optind >= argc) {
+        err_bad_input();
+        return ERROR_CODE;
+    }
+    char *write_path = argv[optind];
+    if (!write_path) {
+        err_bad_input();
+        return ERROR_CODE;
+    }
+    if (access(write_path, F_OK) == 0) {
+        // gut
+    } else {
+        fprintf(stderr, "Error: write file does not exist\n");
+        return ERROR_CODE;
+    }
+
+
+
+    return 0;
 }
